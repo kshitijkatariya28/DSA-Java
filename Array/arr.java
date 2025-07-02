25499d6bf57bdfad17576f5b9277a089948a1c70
kshitij <kshitijkatariya1@gmail.com>
2025-07-02 13:08:37 +0530

    First commit

--- Array/arr.java
+++
@@ -1,7 +0,0 @@
-public class arr {
-    public static void main(String[] args) {
-        int []arr1=new int[5];
-        System.out.println(arr1[0]);
-        System.out.println(arr1[1]);
-    }
-}
